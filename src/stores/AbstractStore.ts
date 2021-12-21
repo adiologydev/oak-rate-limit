e8222b9998a48e4d67db91464840a3636ee79532
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-12-21 20:57:32 +0100

    added types for redis store

--- src/stores/AbstractStore.ts
+++ src/stores/AbstractStore.ts
@@ -1,7 +1,8 @@
+import { Bulk } from "https://deno.land/x/redis@v0.25.0/mod.ts";
 import type { Ratelimit } from "../types/types.d.ts";
 
 export abstract class Store {
-  public get(_ip: string): Ratelimit | undefined {
+  public get(_ip: string): Ratelimit | Promise<Ratelimit | undefined> | undefined {
     throw "Not implemented";
   }
 
@@ -13,7 +14,7 @@ export abstract class Store {
     throw "Not implemented";
   }
 
-  public has(_ip: string): boolean {
+  public has(_ip: string): boolean | Promise<number> {
     throw "Not implemented";
   }
 }
