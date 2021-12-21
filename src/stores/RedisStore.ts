e2ee74d84face2d35ba167e2706e0dbad0d3f18f
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-12-21 20:58:26 +0100

    added redisstore

---
+++ src/stores/RedisStore.ts
@@ -0,0 +1,36 @@
+import { connect } from "../../deps.ts";
+import type { Ratelimit } from "../types/types.d.ts";
+import { Store } from "./AbstractStore.ts";
+
+type Bulk<T> = BulkString | BulkNil;
+type BulkString = string;
+type BulkNil = string;
+
+export class RedisStore extends Store {
+    private readonly store;
+
+    constructor(hostname?: string, port?: number) {
+        super();
+        this.store = connect({
+            hostname: hostname ?? "localhost",
+            port
+        })
+    }
+
+    public async get(ip: string) {
+        // @ts-ignore
+        return JSON.parse(await (await this.store).get(ip))
+    }
+
+    public async set(ip: string, ratelimit: Ratelimit) {
+        (await this.store).set(ip, JSON.stringify(ratelimit));
+    }
+
+    public async delete(ip: string) {
+        (await this.store).del(ip);
+    }
+
+    public async has(ip: string) {
+        return await (await this.store).exists(ip)
+    }
+}
