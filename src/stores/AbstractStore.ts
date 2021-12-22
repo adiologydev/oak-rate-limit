5d1222bc9567eb91eb4a4d8cd547eb46427a1bf8
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-12-23 00:40:22 +0100

    updated types for RedisStore

--- src/stores/AbstractStore.ts
+++ src/stores/AbstractStore.ts
@@ -1,19 +1,19 @@
 import type { Ratelimit } from "../types/types.d.ts";
 
 export abstract class Store {
-  public get(_ip: string): Ratelimit | undefined {
+  public get(_ip: string): Ratelimit | Promise<Ratelimit> | undefined {
     throw "Not implemented";
   }
 
-  public set(_ip: string, _ratelimit: Ratelimit): void {
+  public set(_ip: string, _ratelimit: Ratelimit): void | Promise<void> {
     throw "Not implemented";
   }
 
-  public delete(_ip: string): void {
+  public delete(_ip: string): void | Promise<void> {
     throw "Not implemented";
   }
 
-  public has(_ip: string): boolean {
+  public has(_ip: string): boolean | Promise<boolean> {
     throw "Not implemented";
   }
 }
