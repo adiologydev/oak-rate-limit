07366fb18cf4d11da18b09b5e02b19f5bf8c6168
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-12-23 22:38:59 +0100

    updated returns and bools

--- src/stores/RedisStore.ts
+++ src/stores/RedisStore.ts
@@ -1,4 +1,7 @@
-import { connect, RedisConnectOptions } from "https://deno.land/x/redis@v0.25.0/mod.ts"
+import {
+    connect,
+    RedisConnectOptions,
+} from "https://deno.land/x/redis@v0.25.0/mod.ts";
 import type { Ratelimit } from "../types/types.d.ts";
 import { Store } from "./AbstractStore.ts";
 
@@ -16,25 +19,29 @@ export class RedisStore extends Store {
             name: options.name,
             maxRetryCount: options.maxRetryCount,
             retryInterval: options.retryInterval,
-        })
+        });
     }
 
     public async get(ip: string) {
-        const data = await (await this.store).get(ip)
-        if (!data) return
-        return JSON.parse(data)
+        const data = await (await this.store).get(ip);
+        if (!data) return;
+        return JSON.parse(data);
     }
 
     public async set(ip: string, ratelimit: Ratelimit) {
-        (await this.store).set(ip, JSON.stringify(ratelimit));
+        const newRatelimit = await (
+            await this.store
+        ).set(ip, JSON.stringify(ratelimit));
+        return JSON.parse(newRatelimit);
     }
 
     public async delete(ip: string) {
-        (await this.store).del(ip);
+        const value = Boolean((await this.store).del(ip));
+        return value;
     }
 
     public async has(ip: string) {
-        const VALUE = (await this.store).exists(ip)
-        return !!await VALUE
+        const value = Boolean((await this.store).exists(ip));
+        return value;
     }
 }
